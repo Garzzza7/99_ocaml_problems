26be0f5e6001fdf031b37e5f5b25d12717dd8b2f
Garzzza7 <piotr.czapela@gmail.com>
2025-12-05 14:06:46 +0100

    changes

--- 1.ml
+++ 1.ml
@@ -1,4 +1,72 @@
-let rec last = function [] -> -1 (* idk tbh *) | [ x ] -> x | _ :: x -> last x
-let arr : int list = [ 1; 2; 3; 4; 5; 6; 69; 420; 2137 ];;
+let rec my_last = function
+  | [] -> -1 (* idk tbh *)
+  | [ x ] -> x
+  | _ :: x -> my_last x
+;;
 
-print_int (last arr)
+let rec good_last = function
+  | [] -> None
+  | [ x ] -> Some x
+  | _ :: x -> good_last x
+;;
+
+let good_first = function
+  | x :: xx -> Some x
+  | [] -> None
+;;
+
+let print_atom = function
+  | Some x ->
+    print_int x;
+    print_char '\n'
+  | None -> print_string "invalid"
+;;
+
+let rec nth (n : int) = function
+  | [] -> None
+  | x :: xx -> if n = 0 then Some x else nth (n - 1) xx
+;;
+
+let rec fact (n : int) = if n == 1 then n else n * fact (n - 1)
+
+let rec binom (n : int) (k : int) : int =
+  if n == k then 1 else if k == 1 then n else binom (n - 1) (k - 1) + binom (n - 1) k
+;;
+
+let cos = function
+  | [ x ] -> [ x ]
+  | [] -> []
+  | x -> x
+;;
+
+let rec print_int_arr : int list -> unit = function
+  | x :: xx ->
+    print_int x;
+    print_char ' ';
+    print_int_arr xx
+  | [] -> print_char '\n'
+;;
+
+let rec print_char_arr : char list -> unit = function
+  | x :: xx ->
+    print_char x;
+    print_char ' ';
+    print_char_arr xx
+  | [] -> print_char '\n'
+;;
+
+let arr : int list = [ 23498; 1; 2; 3; 4; 5; 6; 69; 420; 2137; 123123 ]
+let arr1 : char list = [ 'a'; 'a'; 'a'; 'a'; 'a'; 'b' ]
+let arr2 : int list = [ 69 ];;
+
+print_int (my_last arr);
+print_char '\n';
+print_atom (good_first arr2);
+print_char '\n';
+print_int_arr arr;
+print_char_arr arr1;
+print_atom (nth 0 arr);
+print_int (fact 5);
+print_char '\n';
+print_int (binom 12 8);
+print_char '\n'
--- 2.ml
+++ 2.ml
@@ -2,15 +2,17 @@ let rec print_list = function
   (* helper to print a list *)
   | [] -> ()
   | h :: t ->
-      print_int h;
-      print_string " ";
-      print_list t
+    print_int h;
+    print_string " ";
+    print_list t
+;;
 
 let rec last_two = function
   | [] -> [ -1; -1 ] (* idk tbh *)
   | [ x ] -> [ -1; -1 ] (* idk tbh v2 *)
   | [ x; y ] -> [ x; y ]
   | _ :: x -> last_two x
+;;
 
 let arr : int list = [ 1; 2; 3; 4; 5; 6; 69; 420; 2137 ];;
 
